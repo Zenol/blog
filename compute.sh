9c57c7002851b6f919d8ca3f927f060a690366e6
Zenol <jeremy.cochoy@gmail.com>
2015-12-05 22:30:02 +0100

    Change compute script to use header and footers

--- compute.sh
+++ compute.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-GITREPO=/var/www/site/data/blog/
-SITE=/var/www/site/blog/
+GITREPO=/var/www/blog_data/
+BLOG=/var/www/site/blog/
+SITE=/var/www/
 
 #File containing the list of posts
 HF=$GITREPO"tmp.compute.md"
-OUTHF=$SITE"/index.htm"
+OUTHF=$BLOG"/index.html"
 
 # --------------------------------------------
 # - HF
 touch $HF
+echo "# Documents disponibles \/ Documents available" > $HF
 echo "## List of pages / Liste des pages" > $HF
 echo >> $HF
-echo "  * [Liste des developpements d'agregation](/site/blog/pages/agreg_dev.htm)" >> $HF
+echo "  * [Liste des developpements d'agregation](/site/blog/pages/agreg_dev.htmt)" >> $HF
 echo >> $HF
 echo "## List of posts / Liste des billets" >> $HF
 echo >> $HF
@@ -20,17 +22,26 @@ echo >> $HF
 # --------------------------------------------
 # - Posts
 function pan {
-    # echo "pandoc -s --mathjax $1 -A footer.html -c normalize.css -c bootstrap.css -o $2"
-    tail -n +4 $1 | pandoc -s --mathjax -c ../../normalize.css -c ../../bootstrap.css -A $SITE"/footer.htm" -t html5 -o $SITE$2
+    echo "pandoc -s --mathjax $1 -B header.html -A footer.html -c bootstrap.css -o $2"
+    tail -n +4 $1 | pandoc -s --mathjax -c $SITE/bootstrap/bootstrap.min.css -c $SITE/style.css -A $SITE/footer.html -B $SITE/header.html -t html5 -o $BLOG$2
     # Post processing
-    if [ -f $SITE$2 ]; then
+    if [ -f $BLOG$2 ]; then
 	TITLE=$(head -n 1 $1 | cut -d ' ' -f 2-)
 	DATE=$(head -n 3 $1 | tail -n 1 | cut -d ' ' -f 2-)
 	DATEF=$(echo $DATE | sed "s,/,\\\/,g")
 	echo $TITLE" - "$DATE
-	sed -i "s/<body>/<body><div class=\"container\"><div class=\"page-header label\"><h1>$TITLE - "$DATEF"<\/h1><\/div>/g" $SITE$2
-	sed -i "s/<footer id/<\/div><footer id/g" $SITE$2
-	sed -i "s/<title><\/title>/<title>$TITLE<\/title>/g" $SITE$2
+	# H3 -> H4
+	sed -i "s#<h3#<h4#g" $BLOG$2
+	sed -i "s#</h3>#</h4>#g" $BLOG$2
+	# H2 -> H3
+	sed -i "s#<h2#<h3#g" $BLOG$2
+	sed -i "s#</h2>#</h3>#g" $BLOG$2
+	# H1 -> H2
+	sed -i "s#<h1#<h2#g" $BLOG$2
+	sed -i "s#</h1>#</h2>#g" $BLOG$2
+
+	sed -i "s#<!-- Content : -->#<div class=\"page-header\"><h1>$TITLE - $DATEF</h1></div>#g" $BLOG$2
+	sed -i "s#<title></title>#<title>$TITLE</title>#g" $BLOG$2
 
     fi
     # Add to post list :
@@ -43,21 +54,21 @@ git pull
 
 echo "Entering posts"
 cd posts
-for i in $( ls); do
+for i in $( ls -t); do
     if [ -d $i ]; then
 	echo "Entering $i"
 	cd $i
-	mkdir -p $SITE"posts/"$i
+	mkdir -p $BLOG"posts/"$i
 	if [ -f "fr.md" ]; then
 	    echo "	fr.md"
-	    pan "fr.md" "posts/"$i"/fr.htm" "FR"
+	    pan "fr.md" "posts/"$i"/fr.html" "FR"
 	fi
 	if [ -f "en.md" ]; then
 	    echo "	en.md"
-    	    pan "en.md" "posts/"$i"/en.htm" "EN"
+    	    pan "en.md" "posts/"$i"/en.html" "EN"
 	fi
 	if [ -d "data" ]; then
-	    DIR=$SITE"posts/"$i"/"
+	    DIR=$BLOG"posts/"$i"/"
 	    mkdir -p $DIR"data/"
 	    cp -r "data" $DIR
 	    echo "cp -r \"data\" $DIR"
@@ -70,7 +81,7 @@ cd ..
 # --------------------------------------------
 # - HF
 
-pandoc -s $HF -c normalize.css -c bootstrap.css -A $SITE"/footer.htm" -t html5 -o $OUTHF
-sed -i "s/<body>/<body><div class=\"container\"><h1>Documents disponibles \/ Documents available<\/h1>/g" $OUTHF
-sed -i "s/<footer/<\/div><footer/g" $OUTHF
-sed -i 's/<head>/<head><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1">/g' $OUTHF
+pandoc -s $HF -c $SITE/style.css -c $SITE/bootstrap/bootstrap.min.css -A $SITE/footer.html -B $SITE/header.html -t html5 -o $OUTHF
+#Change the dongle to Teaching
+sed -i "s!<li class=\"active\"><a href=\"/\">Home</a></li>!<li><a href=\"/\">Home</a></li>!g" $OUTHF
+sed -i "s!<li><a href=\"/site/blog/\">Blog</a></li>!<li class=\"active\"><a href=\"/site/blog/\">Blog</a></li>!g" $OUTHF
